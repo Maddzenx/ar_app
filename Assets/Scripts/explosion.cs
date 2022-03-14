1cbe15752b794d3f4e6074e86adb36be2ad12d3d
Maifblood <maifblood@gmail.com>
2022-03-14 01:38:06 +0100

    Fler fungerande knappar
    Skapat och sammankopplat nya wireframes

---
+++ Assets/Scripts/explosion.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(UIDocument))]
+
+public class explosion : MonoBehaviour
+{
+    public Button Exp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+    
+        Exp = root.Q<Button>("explosion_view");
+        Exp.clicked += ExpPressed;
+    }
+
+    // Update is called once per frame
+    void ExpPressed()
+    {
+        SceneManager.LoadScene("explosion_view");
+    }
+
+}
