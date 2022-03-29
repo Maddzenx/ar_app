c4ed881b46a02f1488c9a992ac34af7917aff508
Anderssonlinnea <97900517+Anderssonlinnea@users.noreply.github.com>
2022-03-29 08:11:49 +0200

    rotation funkar

---
+++ Assets/Scripts/snurra.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snurra : MonoBehaviour
+{
+    private Touch touch;
+
+    private Vector2 touchPosition;
+
+    private Quaternion rotationX;
+
+    private float rotateSpeedModifier = 0.1f;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                rotationX = Quaternion.Euler(
+                    touch.deltaPosition.y * rotateSpeedModifier,
+                    0f,
+                    0f);
+
+                transform.rotation = rotationX * transform.rotation;
+            }
+        }
+    }
+}
